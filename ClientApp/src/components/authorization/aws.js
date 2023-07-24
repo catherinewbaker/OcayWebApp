34883e24fc2ff82d26c54a7a8a9c8cc21fcd948f
will0608 <wooseok.chung3@gmail.com>
2023-07-24 13:53:42 -0400

    I do not wish fix 7/24

--- ClientApp/src/components/authorization/aws.js
+++ ClientApp/src/components/authorization/aws.js
@@ -1,8 +1,8 @@
 ﻿import AWS from 'aws-sdk';
 
 AWS.config.update({
-    accessKeyId: '',
-    secretAccessKey: '',
+    accessKeyId: 'AKIASIP3GNIGDG5O4NPW',
+    secretAccessKey: '6O25ezppHHUAms2A1Giy7CrZZKxmoAs2omwDKhgj',
     region: 'us-east-1' // Update with your desired region
 });
 
--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -224,6 +224,7 @@ namespace OcayProject.Controllers
                     { "Twice a day", 6.67m },
                     { "Three times a day", 10m },
                     { "More than three times a day", 6.67m }
+                    { "I do not wish to answer.", 0m }
                 };
 
 
@@ -242,6 +243,7 @@ namespace OcayProject.Controllers
                     { "Sometimes", 6.67m },
                     { "Many times", 3.33m },
                     { "Yes", 0m }
+                    { "I do not wish to answer.", 0m }
                 };
 
                 foreach (string answer in request.Q5)
@@ -345,6 +347,7 @@ namespace OcayProject.Controllers
                     { "Every day", 10m },
                     { "Most of the week", 6.67m },
                     { "Less than half of the week", 3.33m }
+                    { "I do not wish to answer.", 0m }
                 };
 
                 foreach (string answer in request.Q11)
